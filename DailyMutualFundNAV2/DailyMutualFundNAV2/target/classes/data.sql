61e21ee711bd55cdfe6ec72d38eb5d264f2972cc
Prat14 <49914688+Prat14@users.noreply.github.com>
2021-08-17 11:13:39 +0530

    Add files via upload

---
+++ DailyMutualFundNAV2/DailyMutualFundNAV2/target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into mutual_fund values ('AXIS','MAxis Bluechip fund',9800.0);
+insert into mutual_fund values ('DSP','MDSP Midcap Fund',6000.0);
+insert into mutual_fund values ('SBI','MSBI Small Cap Fund',4300.0);
